6c69b4c5ddc66087e1f64ceb0d85e4813f7f7fa0
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-03-23 10:43:29 +0700

    add file

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {FormsModule} from "@angular/forms";
+import { ShowPetInfoComponent } from './show-pet-info/show-pet-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent,
+    ShowPetInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/show-pet-info/show-pet-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowPetInfoComponent } from './show-pet-info.component';
+
+describe('ShowPetInfoComponent', () => {
+  let component: ShowPetInfoComponent;
+  let fixture: ComponentFixture<ShowPetInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowPetInfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowPetInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
