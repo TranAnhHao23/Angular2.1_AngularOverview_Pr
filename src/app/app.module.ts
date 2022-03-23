6fb5efd4cd123448117a792cf112a1842ad206a4
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-03-23 17:01:03 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import {FormsModule} from "@angular/forms";
 import { ShowPetInfoComponent } from './show-pet-info/show-pet-info.component';
 import {AngularUsingBootstrapComponent} from "./angular-using-bootstrap/angular-using-bootstrap.component";
 import { SingleCalculatorComponent } from './single-calculator/single-calculator.component';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { SingleCalculatorComponent } from './single-calculator/single-calculator
     FontSizeEditorComponent,
     ShowPetInfoComponent,
     AngularUsingBootstrapComponent,
-    SingleCalculatorComponent
+    SingleCalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
